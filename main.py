1120e41fd184f59b33acdce81fef9ca7eee4c4fa
Cfrazilus1 <99999698+Cfrazilus1@users.noreply.github.com>
2022-02-25 23:47:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def hello(name):
+  print(f"Hello {name}!")
+
+hello("John")
+
+def add_numbers(num1,num2):
+  print(num1 + num2)
+
+add_numbers(4,8)
+add_numbers(3,7)
+
+def dog_info(age,name):
+  print(f"Hi my name is {name} and I am {age} years old!")
+
+dog_info (3, "Spike")
